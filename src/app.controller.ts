39e288f040dafb48e357220b4a987392a90842d9
darkie-dk <kevendaniel.kd@gmail.com>
2025-03-06 03:01:10 -0300

    prisma conn

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PrismaService } from 'prisma/prisma/prisma.service'
 
-@Controller()
+@Controller('/api')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  @Get('/users')
+  async store() {
+    return await this.prisma.user.findMany()
   }
 }
